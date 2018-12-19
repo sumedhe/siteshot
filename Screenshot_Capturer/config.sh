be818d6642ab37e3c70e62b16e820630cfd34b8f
Sumedhe Dissanayake <sumedhedissanayake@gmail.com>
2018-12-20 02:25:52 +0530

    Create setup script

---
+++ Screenshot_Capturer/config.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+PHANTOMJS_DIR=./phantomjs-2.1.1/bin
+OUTPUT_DIR=./output
+INPUT_LIST=./list.txt
---
+++ Screenshot_Capturer/setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Load Phantomjs (Old versions didn't work)
+wget https://github.com/ariya/phantomjs/archive/2.1.1.tar.gz
+tar -xvzf 2.1.1.tar.gz
