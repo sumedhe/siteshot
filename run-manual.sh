a2adb0e696f3b147da87cdc8cefc8c42a7f97b38
Sumedhe Dissanayake <sumedhedissanayake@gmail.com>
2020-01-19 11:33:05 +0530

    Modify run manual script

--- run-manual.sh
+++ run-manual.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
 
-# Load config variables
-source config.sh
+WORKSPACE_DIR=workspace
+CAPTURED_DIR=${WORKSPACE_DIR}/captured
+URL_LIST=resources/top-1m.csv
+
+INTERVAL=20
 
 # Message
 echo "Caputured items will be removed from the list.."
 
 # Capture screenshots for each in list
 # for item in `cat list.txt`;
-for item in `cat resources/top-1m.csv`;
+for item in `cat ${URL_LIST}`;
 
 do 
     rank=${item%,*}
     url=https://${item#*,}
-    filename=output2/${rank}_${item#*,}
+    filename=${CAPTURED_DIR}/${rank}_${item#*,}
 
     # sed '1d' ${INPUT_LIST} > tmp.txt
     # mv tmp.txt ${INPUT_LIST}
 
     if [ "$rank" -gt "$1" ]; then
         open ${url}
-        sleep 20
+        sleep ${INTERVAL}
         # open ${url} && sleep 1 # Switch to window before capturing
         screencapture -x ${filename}.png
     fi
